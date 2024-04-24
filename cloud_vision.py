c1f744cd21350264dd8bd4d6986e2dde45c827b1
clickCA <chanakorn.aramsak@gmail.com>
2024-04-24 09:38:37 +0700

    implement api

--- cloud_vision.py
+++ cloud_vision.py
@@ -1,12 +1,12 @@
 import base64
 import os
-
+from google.cloud import vision
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "superb-robot-420515-356ed151ac07.json"
 
 
 def detect_landmarks(path):
     """Detects landmarks in the file."""
-    from google.cloud import vision
+   
 
     client = vision.ImageAnnotatorClient()
 
@@ -33,9 +33,9 @@ def detect_landmarks(path):
     result["longtitude"] = loc[0].lat_lng.longitude
     return result
 
-
-# if image is detectable
-# return {'landmark': 'Taj Mahal', 'latitude': 27.175144799999998, 'longtitude': 78.04214219999999} else return None
-for i in range(1, 11):
-    name = f"./test_img/test{i}.jpg"
-    print(detect_landmarks(name))
+if __name__ == "__main__":
+    # if image is detectable
+    # return {'landmark': 'Taj Mahal', 'latitude': 27.175144799999998, 'longtitude': 78.04214219999999} else return None
+    for i in range(1, 11):
+        name = f"./test_img/test{i}.jpg"
+        print(detect_landmarks(name))
--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 from typing import Union
 
-from fastapi import FastAPI
-
+import os
+from cloud_vision import detect_landmarks
+from fastapi import FastAPI, File, UploadFile,HTTPException
+from fastapi.responses import FileResponse
+from gemini import generate_gemini_content
+from text_to_speech import text_to_speech
 app = FastAPI()
 
-
 @app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+async def get_api_sample():
+    return {"message": "Welcome to the Geographically-Gifted-Giraffe-GGG API!"}
+@app.post("/find_place")
+async def find_place(file_path):
+    result = detect_landmarks(file_path)
+    if result is None:
+        return {
+  "landmark": "This is a magical place!",
+  "latitude": 48.8588443,
+  "longtitude": 2.2943506
+}
+        # raise HTTPException(status_code=404, detail="Cannot detect location")
+    return result
+
+@app.get("/gemini/{place}")
+async def get_gemini_content(place: str):
+    result = generate_gemini_content(place)
+    return {"gemini_content": result}
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
+@app.post("/text_to_speech")
+async def get_text_to_speech(text: str):
+    try:
+        file_path = text_to_speech(text)
+        return FileResponse(file_path, media_type="audio/wav")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
