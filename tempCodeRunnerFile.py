186c410712111b474e06f3e96c055408d74e02bd
clickCA <chanakorn.aramsak@gmail.com>
2024-04-24 10:13:21 +0700

    remove unused

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-from tkinter import Tk     # from tkinter import Tk for Python 3.x
-# from tkinter.filedialog import askopenfilename
-# from tkintermd.frame import TkintermdFrame
-# Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-# filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
